73674690bba61ceb6edf82a02897e1d3176299e7
Piotr Piórkowski <piotr.piorkowski98@o2.pl>
2020-10-24 15:06:09 +0200

    add message thread methods to the API

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -60,6 +60,19 @@ namespace BST_Projekt.Controllers
             return Ok(messages);
         }
 
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messagesFromRepo = await _repo.GetMessageThread(userId, recipientId);
+
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            return Ok(messageThread);
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId,
             MessageForCreationDto messageForCreationDto)
