18cf3806f7d22d4bb6ebf6b8f5c47ae252fb3a32
Piotr Piórkowski <piotr.piorkowski98@o2.pl>
2020-11-03 23:52:32 +0100

    add send message functionality

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -77,8 +77,9 @@ namespace BST_Projekt.Controllers
         public async Task<IActionResult> CreateMessage(int userId,
             MessageForCreationDto messageForCreationDto)
         {
+            var sender = await _repo.GetUser(userId);
 
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
             messageForCreationDto.SenderId = userId;
@@ -94,7 +95,7 @@ namespace BST_Projekt.Controllers
 
             if (await _repo.SaveAll())
             {
-                var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
                 return CreatedAtRoute("GetMessage",
                     new { userId, id = message.Id }, messageToReturn);
             }
