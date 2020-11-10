42c8b2890be8a546ca050e2064d48c2bf3863d45
Piotr Piórkowski <piotr.piorkowski98@o2.pl>
2020-11-10 13:31:16 +0100

    pass data out of the modal and hook up to API

--- ClientApp/src/app/_components/roles-modal/roles-modal.component.ts
+++ ClientApp/src/app/_components/roles-modal/roles-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { User } from '../../_models/user';
 
@@ -8,6 +8,7 @@ import { User } from '../../_models/user';
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
+  @Output() updateSelectedRoles = new EventEmitter();
   user: User;
   roles: any[];
 
@@ -15,5 +16,11 @@ export class RolesModalComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
+  }
+
 
 }
--- ClientApp/src/app/_services/admin.service.ts
+++ ClientApp/src/app/_services/admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class AdminService {
 
   getUsersWithRoles() {
     return this.http.get(this.baseUrl + 'admin/userswithroles');
+  }
+
+  updateUserRoles(user: User, roles: {}) {
+    return this.http.post(this.baseUrl + 'admin/editRoles/' + user.userName, roles);
   }
 }
